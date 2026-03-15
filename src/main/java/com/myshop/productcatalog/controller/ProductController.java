f06da6cdf3ad21d5949a80311a1715921e1efe44
himanshukumar885 <himanshukumar885@gmail.com>
2026-03-15 13:28:40 +0530

    Added PUT API - CRUD complete!

--- src/main/java/com/myshop/productcatalog/controller/ProductController.java
+++ src/main/java/com/myshop/productcatalog/controller/ProductController.java
@@ -16,14 +16,23 @@ public class ProductController {
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
+
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable Long id) {
         return productService.getProductById(id);
     }
+
     @PostMapping
     public Product addProduct(@RequestBody Product product) {
         return productService.saveProduct(product);
     }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id,
+            @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+    }
+
     @DeleteMapping("/{id}")
     public String deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
--- src/main/java/com/myshop/productcatalog/service/ProductService.java
+++ src/main/java/com/myshop/productcatalog/service/ProductService.java
@@ -22,6 +22,18 @@ public class ProductService {
     public Product saveProduct( Product product) {
         return productRepository.save(product);
     }
+    public Product updateProduct(Long id, Product updatedProduct) {
+    Product existingProduct = productRepository.findById(id).orElse(null);
+    
+    if (existingProduct != null) {
+        existingProduct.setName(updatedProduct.getName());
+        existingProduct.setDescription(updatedProduct.getDescription());
+        existingProduct.setPrice(updatedProduct.getPrice());
+        return productRepository.save(existingProduct);
+    }
+    
+    return null;
+}
     public void deleteProduct(Long id) {
         productRepository.deleteById(id);
     }
