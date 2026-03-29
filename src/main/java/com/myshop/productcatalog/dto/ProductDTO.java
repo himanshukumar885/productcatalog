89e39c348bc7df5dd47dbcbfed3aeb49de8473e7
himanshukumar885 <himanshukumar885@gmail.com>
2026-03-29 14:25:44 +0530

    Added Lombok - cleaner code!

--- src/main/java/com/myshop/productcatalog/dto/ProductDTO.java
+++ src/main/java/com/myshop/productcatalog/dto/ProductDTO.java
@@ -1,35 +1,16 @@
 package com.myshop.productcatalog.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
 
     private Long id;
     private String name;
     private String description;
     private Double price;
-
-    // Constructor
-    public ProductDTO(Long id, String name,
-            String description, Double price) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-    }
-
-    // Getters only! No setters needed!
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
 }
